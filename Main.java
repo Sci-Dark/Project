fe2df56dded87e78f7503c9eaf973de376546e3e
Sci__Dark <94867823+Sci-Dark@users.noreply.github.com>
2021-11-23 14:19:57 -0800

    Main project
    add a person class with a String variable called "name" in a new brach and make a pull request. I will review the code and if it works, you pass the test and the branch gets merged.

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+  static void main(string[ ] args) {
+    Scanner input = new Scanner();
+    person Me = new person();
+    person Big = new person();
+    Me.name = input.nextLine();
+    Big.name = input.nextLine();
+    System.out.println(Big.name);
+    System.out.println(Me.name);
+  }
+}
